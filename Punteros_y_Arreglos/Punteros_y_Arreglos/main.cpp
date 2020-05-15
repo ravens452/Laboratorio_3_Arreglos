0af8b462f572c4c34e53c4a20ccf3e6e3fa51b68
Andrade Guzman <ravens452@gmail.com>
2020-05-15 10:52:50 -0500

    Iniciando Proyecto

---
+++ Punteros_y_Arreglos/Punteros_y_Arreglos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Punteros_y_Arreglos
+//
+//  Created by carlos andrade guzman on 5/15/20.
+//  Copyright © 2020 carlos andrade guzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
