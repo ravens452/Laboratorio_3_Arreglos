b741b37255604a4efb1608a350485c8e455e6b15
Andrade Guzman <ravens452@gmail.com>
2020-05-16 09:45:27 -0500

    Ejercicio 4 Completado

--- Punteros_y_Arreglos/Punteros_y_Arreglos/Ejercicio4.cpp
+++ Punteros_y_Arreglos/Punteros_y_Arreglos/Ejercicio4.cpp
@@ -7,3 +7,20 @@
 //
 
 //Implemente una función que reciba una cadena y retorne su tamaño. (Iterativa y recursiva)
+#include <iostream>
+using namespace std;
+
+int longitud(char *s1){
+    int len = 0;
+    while (*s1 != '\0'){        //Mientras no sea el final de la cadena.
+        len++;s1++;             //Cadena s2 recibira valor de s1 por iteracion.
+        
+    }
+    return len;
+}
+int main(){
+    char a[50];
+    cout<<"Escriba la cadena a: "<<endl;cin>>a;
+    cout<<"La longitud de la cadena es: "<<longitud(a)<<endl;
+
+}
