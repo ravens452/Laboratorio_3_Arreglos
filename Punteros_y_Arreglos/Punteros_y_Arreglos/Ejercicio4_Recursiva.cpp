195f8d2be5c788d4ef6f4d910059fca5b4e99110
Andrade Guzman <ravens452@gmail.com>
2020-05-17 09:35:08 -0500

    ejercicio4 - recursive Hallar el tamaño de la cadena

--- Punteros_y_Arreglos/Punteros_y_Arreglos/Ejercicio4_Recursiva.cpp
+++ Punteros_y_Arreglos/Punteros_y_Arreglos/Ejercicio4_Recursiva.cpp
@@ -9,36 +9,6 @@
 #include <iostream>
 using namespace std;
 
-
-//int longitudArray(char A[])
-//{
-//    int acum =0;
-//    int i;
-//    if (A[acum] == '\0'){
-//        return 0;
-//    }else{
-//        while (i<=0) {
-//            longitudArray(A[0+i]);
-//            acum++;
-//        }
-//    }
-//    return acum;
-//}
-//int haveSameElems(int Arr1[], int size)
-//{
-//  ///pick an element from arr1, if it appears in arr2 cut both out using shiftback
-//  ///if it doesn't appear rerturn false, false should go thorugh all levels till the end
-//  //end: if true check last two elements, if false return false....................................................................................
-//    int suma = 0;
-//  if (size==0)return 0;
-//  else{
-//    haveSameElems(Arr1, size - 1);
-//      cout<<""
-//      return suma;
-////    return (lsearchandshift(Arr2, size, Arr1[size - 1]));//if the last element from Arr1 is \
-//in Arr2 this will be true
-//  }
-//}
 int longitudArray(char *s1)
 {
     int n=0;
